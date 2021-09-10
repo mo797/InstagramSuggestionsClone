6ad0f1712fbab6d454d3988e896df292f33b199c
mo797 <mahmoud.khaled52197@gmail.com>
2021-09-11 00:44:40 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
